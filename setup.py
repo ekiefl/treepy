6827646ae9f171aeaadcb5305df001218cbc07ff
Evan Kiefl <kiefl.evan@gmail.com>
2019-08-09 20:01:53 -0500

    replace requirements.txt with install_requires

--- setup.py
+++ setup.py
@@ -3,13 +3,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='treepy',
-    version="0.5",
+    version="0.6",
     packages=find_packages(),
     author_email = 'kiefl.evan@gmail.com',
     author = 'Evan Kiefl',
     url = 'https://github.com/ekiefl/treepy',
-    install_requires=open('requirements.txt','r').readlines(),
+    install_requires = [
+        'argparse',
+        'tabulate',
+        'colored'
+    ],
     scripts = [os.path.join('bin', 'treepy'),
                os.path.join('bin', 'treepy_python'),
-               os.path.join('bin', 'mls')],
+               os.path.join('bin', 'tls'),
+               os.path.join('bin', 'tls_python'),
+               os.path.join('bin', 'mls'),
+               os.path.join('bin', 'mls_python'),
+    ],
 )
